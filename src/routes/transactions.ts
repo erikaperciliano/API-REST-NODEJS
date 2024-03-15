6bbfc83edaf0b89f1e8ac2a098b6d972548cbc0d
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-03-15 17:28:18 +0000

    add cookies on the Fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,8 @@ import { knex } from '../database'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 
+// Cookies <-> ways to maintain context between requests
+
 export async function transactionRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const transactions = await knex('transactions').select()
@@ -41,10 +43,22 @@ export async function transactionRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return reply.status(201).send()
