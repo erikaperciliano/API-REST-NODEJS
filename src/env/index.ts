7ed4d3dfbfbd529e2b701454278f24e1eb3df089
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-07-25 18:01:47 +0100

    removes default

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,7 +9,7 @@ if(process.env.NODE_ENV === 'test'){
 
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-    DATABASE_CLIENTE: z.enum(['sqlite', 'pg']).default('sqlite'),
+    DATABASE_CLIENTE: z.enum(['sqlite', 'pg']),
     DATABASE_URL: z.string(),
     PORT: z.coerce.number().default(3333)
 })
